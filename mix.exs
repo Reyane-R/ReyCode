8b0436042ee86ff09a5b33296856f1a72846e444
Reyane Remache <reyane.remache@blockversity.xyz>
2026-09-23 15:52:37 +0200

    Bump version to 0.19.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ReyCode.MixProject do
   def project do
     [
       app: :rey_code,
-      version: "0.18.4",
+      version: "0.19.0",
       licenses: ["MIT"],
       elixir: "~> 1.19",
       elixirc_paths: elixirc_paths(Mix.env()),
